2d39d5fc2f3d1274cbccdb4cbaf4648b200a7830
jaeick <jaeick@shin>
2018-09-21 21:15:40 +0900

    인증번호 디비에 추가하여서 그에따른 정보 수정

---
+++ src/com/pkb/member/controller/SendSMS.java
@@ -0,0 +1,43 @@
+package com.pkb.member.controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import net.nurigo.java_sdk.Coolsms;
+
+/**
+ * Servlet implementation class SendSMS
+ */
+@WebServlet("/send.sms")
+public class SendSMS extends HttpServlet {
+	
+	
+    public SendSMS() {
+      
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
