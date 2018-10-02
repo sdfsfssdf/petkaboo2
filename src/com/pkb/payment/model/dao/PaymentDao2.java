59ba7e6c7751f02d2ef78d4b7c557850f95fbe7a
hyennee <kate2178930@gmail.com>
2018-10-02 15:44:02 +0900

    ㅋㅁ

--- src/com/pkb/payment/model/dao/PaymentDao2.java
+++ src/com/pkb/payment/model/dao/PaymentDao2.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Properties;
 
@@ -210,5 +211,115 @@ public class PaymentDao2 {
 			close(pstmt);
 		}
 		return count;
+	}
+
+	public ArrayList<Payment> searchMainInfo(Connection con, int currentPage, int limit, String pay_date,
+			String pay_method) {
+		Statement stmt = null;
+		ResultSet rs = null;
+		ArrayList<Payment> plist = null;
+		Payment p = null;
+		String query = prop.getProperty("selectListInquiry");
+		StringBuilder sb = null;
+		
+		try {
+			stmt = con.prepareStatement(query);
+
+			int startRow = (currentPage - 1) * limit + 1;
+			int endRow = startRow + limit - 1;
+
+			if (!pay_date.equals("all")) {
+				sb = new StringBuilder();
+				if (pay_date.equals("today")) {
+					sb.append(
+							"WHERE TO_CHAR(PAY_DATE,'YYYYmmdd') BETWEEN TO_CHAR(SYSDATE,'YYYYmmdd') AND TO_CHAR(SYSDATE,'YYYYmmdd') ");
+				} else if (pay_date.equals("week")) {
+					sb.append(
+							"WHERE TO_CHAR(PAY_DATE,'YYYYmmdd') BETWEEN TO_CHAR(SYSDATE-7,'YYYYmmdd') AND TO_CHAR(SYSDATE,'YYYYmmdd') ");
+				} else if (pay_date.equals("month")) {
+					sb.append(
+							"WHERE TO_CHAR(PAY_DATE,'YYYYmmdd') BETWEEN TO_CHAR(SYSDATE-30,'YYYYmmdd') AND TO_CHAR(SYSDATE,'YYYYmmdd') ");
+				} else if(pay_date.equals("year")){
+					sb.append(
+							"WHERE TO_CHAR(PAY_DATE,'YYYYmmdd') BETWEEN TO_CHAR(SYSDATE-365,'YYYYmmdd') AND TO_CHAR(SYSDATE,'YYYYmmdd') ");
+				}
+			}
+			if (pay_method.equals("creditCard")) {
+				if (sb == null) {
+					sb = new StringBuilder();
+					sb.append("WHERE PAYMENT_TYPE = 0 ");
+				} else {
+					sb.append("AND PAYMENT_TYPE = 0 ");
+				}
+			} else if (pay_method.equals("bankBook")) {
+				if (sb == null) {
+					sb = new StringBuilder();
+					sb.append("WHERE PAYMENT_TYPE = 1 ");
+				} else {
+					sb.append("AND PAYMENT_TYPE = 1 ");
+				}
+			}
+
+			if (pay_method.equals("use")) {
+				if (sb == null) {
+					sb = new StringBuilder();
+					sb.append("WHERE PAY_METHOD = 'U' ");
+				} else {
+					sb.append("AND PAY_METHOD = 'U' ");
+				}
+			} else if (pay_method.equals("cencel")) {
+				if (sb == null) {
+					sb = new StringBuilder();
+					sb.append("WHERE PAY_METHOD = 'R' ");
+				} else {
+					sb.append("AND PAY_METHOD = 'R' ");
+				}
+			} else if(pay_method.equals("reCharge")) {
+				if (sb == null) {
+					sb = new StringBuilder();
+					sb.append("WHERE PAY_METHOD = 'C' ");
+				} else {
+					sb.append("AND PAY_METHOD = 'C' ");
+				}
+			}
+			
+			if(sb !=null){
+				query = query + sb.toString() + " )) WHERE RNUM BETWEEN "+startRow + " AND " + endRow;
+			} else {
+				query = query + " )) WHERE RNUM BETWEEN "+startRow + " AND " + endRow;
+			}
+
+			System.out.println(query);
+			rs = stmt.executeQuery(query);
+			plist = new ArrayList<Payment>();
+
+			while (rs.next()) {
+				p = new Payment();
+
+				p.setPay_no(rs.getInt("pay_no"));
+				p.setUser_no(rs.getInt("user_no"));
+				p.setPay_amount(rs.getInt("pay_amount"));
+				p.setPay_date(rs.getDate("pay_date"));
+				p.setPay_method(rs.getString("pay_method"));
+				p.setPayment_type(rs.getInt("payment_type"));
+				p.setPayment_cash_status(rs.getInt("payment_cash_status"));
+				p.setPayment_cash_date(rs.getDate("payment_cash_date"));
+				p.setCard_apply_no(rs.getString("card_apply_no"));
+				p.setImp_uid(rs.getString("import_auth_no"));
+				p.setAccount_no(rs.getString("account_no"));
+				p.setEmail(rs.getString("email"));
+
+				plist.add(p);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			close(rs);
+			close(stmt);
+		}
+
+		return plist;
 	}
 }
